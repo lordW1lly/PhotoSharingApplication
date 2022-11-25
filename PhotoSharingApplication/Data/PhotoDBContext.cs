84d2b66a1d47de06159a573c9292a97e92fade2b
lordW1lly <sebastianengelstajn@gmail.com>
2022-11-25 16:21:32 -0300

    Agregar archivos de proyecto.

---
+++ PhotoSharingApplication/Data/PhotoDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using PhotoSharingApplication.Models;
+
+namespace PhotoSharingApplication.Data
+{
+    public class PhotoDBContext : DbContext
+    {
+        public PhotoDBContext() : base("KeyPhotoDB") { }
+
+        public DbSet<Photo> photos { get;set; }
+
+        public DbSet<Comment> comments { get;set; }  
+    }
+}
