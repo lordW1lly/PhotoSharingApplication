d6f7ea3a569ac7fef16065d5092beda5c6a89921
lordW1lly <sebastianengelstajn@gmail.com>
2022-11-28 19:31:02 -0300

    terminando photosharing

---
+++ PhotoSharingApplication/Controllers/PhotoController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Globalization;
+using PhotoSharingApplication.Models;
+using PhotoSharingApplication.Data;
+
+namespace PhotoSharingApplication.Controllers
+{
+    public class PhotoController : Controller
+    {
+        //instancia del context
+        private PhotoDBContext context = new PhotoDBContext();
+
+        // GET: Photo
+        public ActionResult Index()
+        {
+
+            return View("Index");
+        }
+
+        [ChildActionOnly]
+        public ActionResult _PhotoGallery (int number = 0)
+        {
+            List<Photo> photos;
+            if (number == 0)
+            {
+                photos = context.photos.ToList();
+            }
+            else
+            {
+                photos = (
+                from p in context.photos
+                orderby p.CreatedDate descending
+                select p).Take(number).ToList();
+            }
+            return PartialView("_PhotoGallery",photos);
+        }
+
+        public ActionResult Display(int id)
+        {
+            Photo photo = context.photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Display", photo);
+
+        }
+
+        public ActionResult Create()
+        {
+            Photo newPhoto = new Photo();
+            newPhoto.CreatedDate = DateTime.Today;
+            return View("Create", newPhoto);
+
+        }
+
+        [HttpPost]
+        public ActionResult Create (Photo photo, HttpPostedFileBase image)
+        {
+            photo.CreatedDate = DateTime.Today;
+
+            if (!ModelState.IsValid)
+            {
+                return View("Create", photo);
+            }
+            else
+            {
+                if (image != null)
+                {
+                    photo.ImageMimeType =
+                    image.ContentType;
+                    photo.PhotoFile = new
+                    byte[image.ContentLength];
+                    image.InputStream.Read(
+                    photo.PhotoFile, 0,
+                    image.ContentLength);
+                }
+                context.photos.Add(photo);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Photo photo = context.photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View("Delete", photo);
+        }
+
+
+
+
+
+    }
+}
